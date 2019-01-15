0529361b23d4b24b862ae019ffffb9dcdfb09fac
Austin Cumberlander <austincumberlander@gmail.com>
2019-01-14 18:20:27 -0600

    able to create new events. date is broken. will create separate card for it to fix later.

---
+++ src/components/Form/Form.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Form.scss';
+import authRequests from '../../Helpers/Data/authRequests';
+
+const defaultEvent = {
+  uid: '',
+  event: '',
+  startdate: '',
+  location: '',
+};
+
+class Form extends React.Component {
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  }
+
+  state = {
+    newEvent: defaultEvent,
+  }
+
+  formFieldStringState = (name, e) => {
+    e.preventDefault();
+    const tempEvent = { ...this.state.newEvent };
+    tempEvent[name] = e.target.value;
+    this.setState({ newEvent: tempEvent });
+  }
+
+  eventChange = e => this.formFieldStringState('event', e);
+
+  dateChange = e => this.formFieldStringState('startdate', e);
+
+  locationChange = e => this.formFieldStringState('location', e);
+
+  formSubmit = (e) => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    const myEvent = { ...this.state.newEvent };
+    myEvent.uid = authRequests.getCurrentUid();
+    onSubmit(myEvent);
+    this.setState({ newEvent: defaultEvent });
+  }
+
+  render() {
+    const { newEvent } = this.state;
+    const title = () => <h2>Add New Event:</h2>;
+    return (
+      <div className="eventForm col">
+      {title()}
+        <form onSubmit={this.formSubmit}>
+          <div className="form-group">
+            <label htmlFor="event">Event</label>
+            <input
+              type="text"
+              className="form-control"
+              id="event"
+              placeholder="Ayden's Baseball Game"
+              value={newEvent.event}
+              onChange={this.eventChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="startDate">Start Date</label>
+            <input
+              type="date"
+              className="form-control"
+              id="startDate"
+              placeholder="02/23/2019"
+              value={newEvent.startdate}
+              onChange={this.dateChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="location">Location</label>
+            <input
+              type="text"
+              className="form-control"
+              id="location"
+              placeholder="NSS"
+              value={newEvent.location}
+              onChange={this.locationChange}
+            />
+          </div>
+          <button className="btn btn-danger">
+            Save Event
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
--- src/components/Pages/Events/events.js
+++ src/components/Pages/Events/events.js
@@ -3,6 +3,8 @@ import './Events.scss';
 import smashRequests from '../../../Helpers/Data/Requests/smashRequests';
 import SingleEvent from '../SingleEvent/SingleEvent';
 import authRequests from '../../../Helpers/Data/authRequests';
+import Form from '../../Form/Form';
+import eventRequests from '../../../Helpers/Data/Requests/eventRequests';
 
 class Events extends React.Component {
   state = {
@@ -20,6 +22,18 @@ class Events extends React.Component {
       });
   }
 
+  formSubmitEvent = (newEvent) => {
+    eventRequests.postRequest(newEvent)
+      .then(() => {
+        const currentUid = authRequests.getCurrentUid();
+        smashRequests.getEventsFromMeAndFriends(currentUid)
+          .then((events) => {
+            this.setState({ events });
+          });
+      })
+      .catch(err => console.error('error with events post', err));
+  };
+
   render() {
     const { events } = this.state;
     const singleEventComponents = events.map(event => (
@@ -32,7 +46,14 @@ class Events extends React.Component {
       <div className="events col">
         <h2>Events Component</h2>
         <div className="eventsContainer">
-          <div>{singleEventComponents}</div>
+          <div className="eventCards">
+            {singleEventComponents}
+          </div>
+        </div>
+        <div className="addNewEvent">
+          <Form
+            onSubmit={this.formSubmitEvent}
+          />
         </div>
       </div>
     );
